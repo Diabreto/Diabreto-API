157886d9e7983715f3e5b9174d4ffe4eb89f8dcf
pedrocb <pedrocb_c-s@hotmail.com>
2017-02-18 09:59:33 +0000

    records index and show endpoint

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -8,9 +8,26 @@ class RecordsController < ApplicationController
       @errors = @record.errors.full_messages
       render :error, status: :unprocessable_entity
     end
-
   end
 
+  def show
+    @record = Record.find_by_id(params[:id])
+    if @record
+      if current_user.records.exists?(@record.id)
+        render status: :ok
+      else
+        @errors = [t(:user_not_authorized)]
+        render :error, status: :unauthorized
+      end
+    else
+      @errors = [t(:record_not_found)]
+      render :error, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @records = current_user.records
+  end
 
   private
 
