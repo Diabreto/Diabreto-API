c9105081d1b74ce377b7fe26402741f1630bf537
pedrocb <pedrocb_c-s@hotmail.com>
2017-02-18 06:39:06 +0000

    changes user fields to floats

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218025751) do
+ActiveRecord::Schema.define(version: 20170218063218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                   default: "",  null: false
-    t.string   "encrypted_password",      default: "",  null: false
+    t.string   "email",                   default: "",    null: false
+    t.string   "encrypted_password",      default: "",    null: false
     t.string   "authentication_token"
-    t.string   "username",                default: "",  null: false
-    t.integer  "glycemia_unit",           default: 0,   null: false
-    t.integer  "carbohydrates_to_unit",   default: 1,   null: false
-    t.integer  "glycemia_target",         default: 100, null: false
-    t.integer  "insulin_to_unit",         default: 1,   null: false
-    t.integer  "correction_factor",       default: 1,   null: false
-    t.integer  "hyperglycemia_threshold", default: 120, null: false
-    t.integer  "hypoglycemia_threshold",  default: 80,  null: false
+    t.string   "username",                default: "",    null: false
+    t.float    "glycemia_unit",           default: 0.0,   null: false
+    t.float    "carbohydrates_to_unit",   default: 1.0,   null: false
+    t.float    "glycemia_target",         default: 100.0, null: false
+    t.float    "insulin_to_unit",         default: 1.0,   null: false
+    t.float    "correction_factor",       default: 1.0,   null: false
+    t.float    "hyperglycemia_threshold", default: 120.0, null: false
+    t.float    "hypoglycemia_threshold",  default: 80.0,  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           default: 0,   null: false
+    t.integer  "sign_in_count",           default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
